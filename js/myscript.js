366f77397c33f5408d44733a977717f51fdf1263
Vale <oirelav95@gmail.com>
2022-12-01 12:29:58 +0100

    aggiungo risposta utente dopo invio messaggio

--- js/myscript.js
+++ js/myscript.js
@@ -179,7 +179,11 @@ createApp({
             this.activeContact = indice;
         },
         addMessage(indice) {
+
             this.contacts[indice].messages.push({date: '10/01/2020 15:51:00', message: this.newMessage, status: 'sent'});
+            
+            setTimeout(()=> this.contacts[indice].messages.push({date: '10/01/2020 15:51:00', message: "ok", status: 'received'}), 1000);
+            
             this.newMessage = "";
         }
     }
