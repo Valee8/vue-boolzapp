35587228b4b2302d1f2be5fefa6fe6bf2cb387c8
Vale <oirelav95@gmail.com>
2022-12-05 11:09:28 +0100

    aggiungo vari bonus

--- js/myscript.js
+++ js/myscript.js
@@ -24,14 +24,38 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            // Scritta ultimo accesso
+            lastAccess: true,
+            // Scritta online
+            online: false,
+            // Per mostrare il popup che mi fa aggiungere una nuova conversazione
+            showPopup: false, 
+            // Per mostrare il menu dropdown tramite i 3 punti in alto a sinistra   
+            showMenuMyInfo: false,
+            // Contiene il nuovo contatto aggiunto dall'utente
+            newContact: '',
+            // Prende come valore true o false a secondo che il nuovo messaggio abbia spazi vuoti
+            noSpace: '',
+            // Per mostrare la scritta sta scrivendo
+            isWriting: false,
+            // Per far comparire un messaggio d'errore se si invia contenuto vuoto o con uno spazio
             error: false,
+            // Contiene i numeri random che genero
             randomNumber: '',
+            // Risposte random dell'utente
             randomAnswer: ["Ciao!", "No!", "Sei bello", "Oggi sono stanco", "Ci vediamo domani", "A dopo"],
+            randomAvatar: ["img/avatar_1.jpg", "img/avatar_2.jpg", "img/avatar_3.jpg", "img/avatar_4.jpg"],
+            // Data e ora
             dt: DateTime.now(),
+            // Scritta "oggi"
             today: DateTime.now().plus({ days: 0 }).toRelativeCalendar(),
-            isHidden: false,
+            // Per mostrare il menu dropdown tramite i 3 punti in alto a destra
+            showMenuUser: false,
+            // Contiene il testo che inserisco nella ricerca
             search: '',
+            // Contiene il nuovo messaggio che inserisco nell'input
             newMessage: '',
+            // Il contatto attivo
             activeContact: 0,
             contacts: [
                 {
@@ -136,7 +160,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: 'img/avatar_2.jpg',
+                    avatar: 'img/avatar_io.jpg',
                     visible: true,
                     messages: [
                         {
@@ -199,38 +223,154 @@ createApp({
         }
     },
     methods: {
+        // Faccio scomparire i menu dropdown cliccando fuori
+        closeMenu() {
+
+            if (this.showMenuUser) {
+                this.showMenuUser = false;
+            }
+
+            if (this.showMenuMyInfo) {
+                this.showMenuMyInfo = false;
+            }
+            
+        },
+        // Funzione per cambiare contatto attivo
         changeContact(indice) {
             this.activeContact = indice;
         },
+        // Funzione per aggiungere un nuovo messaggio
         addMessage() {
 
-            console.log(this.newMessage.length);
-
+            // Assegno a randomNumber un numero compreso tra primo e ultimo elemento di randomAnswer
             this.randomNumber = this.numCasuale(0, this.randomAnswer.length - 1);
 
-            if (this.newMessage.indexOf(" ") && this.newMessage.length != 0) {
+            // Controllo che il nuovo messaggio non abbia spazi
+            this.noSpace = this.onlySpaces(this.newMessage);
 
+            // Se il nuovo messaggio non ha spazi vuoti o non ha contenuto vuoto 
+            if (!this.noSpace && this.newMessage.length != 0) {
+
+                // Faccio sparire il messaggio d'errore se presente
                 this.error = false;
 
+                // Aggiungo il nuovo messaggio
                 this.contacts[this.activeContact].messages.push({date: `${this.dt.day}/${this.dt.month}/${this.dt.year} ${this.dt.hour}:${this.dt.minute}:${this.dt.second}`, message: this.newMessage, status: 'sent'});
-                            
-                setTimeout(()=> this.contacts[this.activeContact].messages.push({date: `${this.dt.day}/${this.dt.month}/${this.dt.year} ${this.dt.hour}:${this.dt.minute}:${this.dt.second}`, message: this.randomAnswer[this.randomNumber], status: 'received'}), 1000);
+
+                // Faccio apparire la scritta sta scrivendo
+                this.isWriting = true;
+                     
+                // Richiamo funzione che aggiunge la risposta
+                setTimeout(this.newAnswer, 1000);
+
             }
+            // Se ha spazi vuoti o contenuto vuoto faccio comparire il messaggio d'errore
             else {
                 this.error = true;
             }
 
+            // Svuoto contenuto del nuovo messaggio
             this.newMessage = "";
             
         },
+        // Bottone annulla nel popup
+        cancelButton() {
+            if (this.showPopup) {
+                this.showPopup = false;
+            }
+            else {
+                this.showPopup = true;
+            }
+
+            // Svuoto contenuto newContact
+            this.newContact = "";
+        },
+        // Funzione per far scomparire la scritta Online e far apparire ultimo accesso
+        hiddenStatus() {
+
+            this.online = false;
+
+            this.lastAccess = true;
+
+        },
+        // Funzione per eliminare singolo messaggio
         deleteMessage(indice) {
             this.contacts[this.activeContact].messages.splice(indice, 1);
         },
+        // Funzione che ritorna numero casuale tra min e max
         numCasuale(min, max) {
             return (Math.floor(Math.random() * ((max + 1) - min) + min));
+        },
+        // Funzione per la risposta
+        newAnswer() {
+            // Aggiungo risposta dell'utente
+            this.contacts[this.activeContact].messages.push({
+                date: `${this.dt.day}/${this.dt.month}/${this.dt.year} ${this.dt.hour}:${this.dt.minute}:${this.dt.second}`, 
+                message: this.randomAnswer[this.randomNumber], 
+                status: 'received'
+            });
+
+            // Faccio sparire la scritta sta scrivendo
+            this.isWriting = false;
+
+            // Faccio apparire la scritta Online
+            this.online = true;
+
+            // Faccio scomparire scritta ultimo accesso
+            this.lastAccess = false;
+
+            // Richiamo funzione per far scomparire scritta online
+            setTimeout(this.hiddenStatus, 2000);
+
+        },
+        // Funzione per eliminare tutti i messaggi di una chat
+        deleteAllMessages() {
+            this.contacts[this.activeContact].messages.splice(0);
+
+            // Faccio scomparire il menu dropdown se non ci sono messaggi 
+            if(this.contacts[this.activeContact].messages.length === 0) {
+                this.showMenuUser = false;
+            }
+        },
+        // Funzione per eliminare una chat
+        deleteChat() {
+            this.contacts.splice(this.activeContact, 1);
+
+            // Faccio scomparire il menu dropdown dopo che ho eliminato una chat
+            this.showMenuUser = false;
+        },
+        // Funzione che ritorna true o false a seconda che il contenuto di str abbia spazi vuoti o no
+        onlySpaces(str) {
+            return str.trim().length === 0;
+        },
+        // Funzione per aggiungere una nuova conversazione
+        addConversation() {
+
+            // Assegno a randomNumber un numero compreso tra primo e ultimo elemento di randomAvatar
+            this.randomNumber = this.numCasuale(0, this.randomAvatar.length - 1);
+
+            // Faccio apparire il popup
+            this.showPopup = true;
+
+            // Controllo che non ci siano spazi
+            this.noSpace = this.onlySpaces(this.newContact);
+
+            // Se il nuovo messaggio non ha spazi vuoti o non ha contenuto vuoto 
+            if (!this.noSpace && this.newContact.length != 0) {
+
+                // Aggiungo nuova chat
+                this.contacts.unshift({name: this.newContact, avatar: this.randomAvatar[this.randomNumber], visible: true, messages: []});
+
+                // Faccio scomparire il popup
+                this.showPopup = false;
+            }
+
+            // Svuoto il contenuto di newContact
+            this.newContact = "";
         }
     },
     computed: {
+        // Funzione per cercare i contatti
         searchContact(){
             if (this.search) {
             return this.contacts.filter((item) => {
